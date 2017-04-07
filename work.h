b7e1c2b2d28217c0da663d206f4bc0d474457a93
liqing <liqinglq@hfbank.com>
2017-04-07 14:52:41 +0800

    work.h

---
+++ work.h
@@ -0,0 +1,17 @@
+//
+//  SavingSalaryRecordViewController.h
+//  EGBankP2B
+//
+//  Created by fuwenqiang on 16/12/15.
+//  Copyright © 2016年 HFBank. All rights reserved.
+//
+
+#import "UIViewControllerBase.h"
+#import "PassWordViewController.h"
+
+
+@interface SavingSalaryRecordViewController : PassWordViewController
+
+@property (strong,nonatomic) HFR371Res * hfr371res; // 在进入此页的时候传入，
+@property (strong,nonatomic) UIViewController * backViewController;
+@end
